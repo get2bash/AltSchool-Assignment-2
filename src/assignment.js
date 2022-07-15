315557df4c71ea10911d32646a909a637b68f6ba
Isah Bashir Ibrahim <get2bash@gmail.com>
2022-07-15 01:18:10 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+let sum = 0;
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  for(let number of arrayOfNumbers) {
+    sum += number;   
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +31,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+ let count = 0;
+ function countEvenNumbers(arrayOfNumbers) {
+    for(let i = 0; i < arrayOfNumbers.length; i++) {
+        if (arrayOfNumbers[i] % 2 == 0){
+            count ++;
+        } 
+    }
+    return count;
+ } 
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
